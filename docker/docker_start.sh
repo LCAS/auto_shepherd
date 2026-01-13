2cd72b7033c0976b06a5d79433eace6c26ed4d6d
James R Heselden <primordia@live.com>
2026-01-13 17:05:02 +0000

    docker start includes --build now

--- docker/docker_start.sh
+++ docker/docker_start.sh
@@ -15,7 +15,7 @@ xhost +SI:localuser:ros
 # 1) Do we have *any* containers for this Compose project?
 if ! docker compose ps --quiet | grep -q .; then
     echo "No containers found — running 'docker compose up -d' to create them..."
-    docker compose up -d
+    docker compose up -d --build
 fi
 
 # 2) Are any of those containers currently running?
